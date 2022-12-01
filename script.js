066775b8450951d231483e0ecd2b9bc4739e7f73
blossom4488 <113397002+blossom4488@users.noreply.github.com>
2022-11-30 21:54:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function removeImage() {
+  document.getElementById("back-slide").style.visibility = "visible";
+  document.getElementById("first-slide").style.visibility = "hidden";
+}
+
+function addImage() {
+  document.getElementById("back-slide").style.visibility = "hidden";
+  document.getElementById("first-slide").style.visibility = "visible";
+}
